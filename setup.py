5290f5dec613032978887df16b128803a292d809
Brian Rutledge <bhrutledge@gmail.com>
2019-05-08 09:01:13 -0400

    Add URL & author

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setuptools.setup(
     name='trello2md',
     version='0.1.1',
     description='Transform Trello card JSON to Markdown',
+    url='https://github.com/bhrutledge/trello2md',
+    author='Brian Rutledge',
+    author_email='brian@bhrutledge.com',
     py_modules=['trello2md'],
     entry_points={
         'console_scripts': [
