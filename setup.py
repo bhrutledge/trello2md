2ccd6201a5b14cc094b9aa05e201ed2637a841dc
Brian Rutledge <brian@bhrutledge.com>
2019-11-28 19:51:32 -0500

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="trello2md",
-    version="0.1.2",
-    description="Transform Trello card JSON to Markdown",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    url="https://github.com/bhrutledge/trello2md",
-    author="Brian Rutledge",
-    author_email="brian@bhrutledge.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Utilities",
-    ],
-    py_modules=["trello2md"],
-    entry_points={"console_scripts": ["trello2md=trello2md:main"]},
-    extras_require={"dev": ["black", "flake8", "mypy", "pydocstyle"]},
-)
+setup()
